dadf573017c1c5ce31dc58795dcfeb4969dacdd2
Bartosz Wilk <bartek94@gmail.com>
2017-08-20 18:25:50 +0200

    add textarea and test script

--- app/src/app.js
+++ app/src/app.js
@@ -1,10 +1,30 @@
-import {div} from '@cycle/dom'
+import {div, textarea, h} from '@cycle/dom'
 import xs from 'xstream'
 
 export function App (sources) {
-  const vtree$ = xs.of(
-    div('My Awesome Cycle.js app')
-  )
+  let post = '';
+  const vtree$ = sources.DOM.select('textarea').events('input')
+      .map(ev => {
+        switch (ev.inputType) {
+          case "insertLineBreak":
+            post += '\n';
+            break;
+          case "deleteContentBackward":
+            post = post.slice(0,-1);
+            break;
+          default:
+            post += ev.data;
+        }
+
+        document.getElementById('editor').innerHTML = post.replace(/#(.*)\n/g, '<h1>$1</h1>')
+                                                          .replace(/(.*)\n/g, '$1<br>')
+
+        return;
+      })
+      .startWith(post)
+      .map(content =>
+        div([textarea({attrs: { style: 'width: 400px; height: 200px;' }}), div({ attrs: { id: 'editor' } })])
+      )
   const sinks = {
     DOM: vtree$
   }
